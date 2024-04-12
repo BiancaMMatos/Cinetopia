91b76e756cc4073edb7fdaee434ea4d23a21c10f
BiancaMMatos <macielmatosb@gmail.com>
2024-04-11 22:45:58 -0300

    develop: Removendo storyboard

---
+++ Cinetopia/Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Bianca Maciel Matos on 07/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
