56a6d6323bc5eff001a3d2e55944e98fbe0f7186
BiancaMMatos <macielmatosb@gmail.com>
2024-04-11 23:44:01 -0300

    feature/helloWorld: Criando uma propriedade de label na tela

--- Cinetopia/Cinetopia/HomeViewController.swift
+++ Cinetopia/Cinetopia/HomeViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    var label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
+    
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .background
+        
+        // Adding subviews
+        view.addSubview(label)
+        
     }
 
 
 }
-
