6cf07dbaf571dc51333f5fef9fb43d21a8d58b5a
BiancaMMatos <macielmatosb@gmail.com>
2024-04-13 23:54:18 -0300

    feature/criandoUmaTabela: Criando a tableView

--- Cinetopia/Cinetopia/MoviesViewController.swift
+++ Cinetopia/Cinetopia/MoviesViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 class MoviesViewController: UIViewController {
 
     
+    // MARK:- Properties
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear // limpando o fundo, garantindo o fundo com a cor .background ao invés de branco
+        return tableView
+    }()
+    
     // MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .background
+        addSubviews()
+        setupConstraints()
         setupNavigationBar()
     }
     
     // MARK:- Functions
+    private func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
     private func setupNavigationBar() {
         title = "Filmes populares"
         navigationController?.navigationBar.prefersLargeTitles = true
