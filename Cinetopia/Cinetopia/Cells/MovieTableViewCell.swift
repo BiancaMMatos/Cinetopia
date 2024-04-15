de3c83264f6af149a3922a3a3ef8bdbb98625980
BiancaMMatos <macielmatosb@gmail.com>
2024-04-15 19:26:04 -0300

    feature/modelagemDeDados: Criando os posicionamentos

--- Cinetopia/Cinetopia/Cells/MovieTableViewCell.swift
+++ Cinetopia/Cinetopia/Cells/MovieTableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class MovieTableViewCell: UITableViewCell {
 
@@ -19,11 +20,33 @@ class MovieTableViewCell: UITableViewCell {
         return imageView
     }()
     
+    private lazy var movieTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Avatar"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private lazy var movieReleaseDateLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Lançamento: 18/12/2009"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .white.withAlphaComponent(0.75)
+        
+        return label
+    }()
+    
+    
     // MARK:- Life Cycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) { // igual ao ViewDidLoad
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addSubviews()
         setupConstraints()
+        backgroundColor = .background
     }
     
     required init?(coder: NSCoder) {
@@ -42,10 +65,37 @@ class MovieTableViewCell: UITableViewCell {
     // MARK:- Functions
     private func addSubviews() {
         addSubview(moviePosterImageView)
+        addSubview(movieTitleLabel)
+        addSubview(movieReleaseDateLabel)
     }
     
     private func setupConstraints() {
+        /// Poster
+        moviePosterImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.bottom.equalToSuperview().offset(-16)
+            make.width.equalToSuperview().multipliedBy(0.3) // largura relativa
+        }
         
+        /// Title label
+        movieTitleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalTo(moviePosterImageView.snp.centerY)
+            make.leading.equalTo(moviePosterImageView.snp.trailing).offset(16)
+        }
+        
+        /// Movie Release
+            movieReleaseDateLabel.snp.makeConstraints { make in
+                make.top.equalTo(movieTitleLabel.snp.bottom).offset(8) // Distância entre o título e a data de lançamento
+                make.leading.equalTo(moviePosterImageView.snp.trailing).offset(16)
+                make.trailing.lessThanOrEqualToSuperview().offset(-16) // Garante que o texto completo seja visível
+                make.bottom.lessThanOrEqualToSuperview().offset(-16) // Garante que a data não ultrapasse a parte inferior da tela
+            }
     }
 
+}
+
+#Preview {
+    MovieTableViewCell()
 }
