d3d45d0bad71d440de843cac5c2f96ce9dd49377
BiancaMMatos <macielmatosb@gmail.com>
2024-04-14 21:49:57 -0300

    Criando estrutura para os filmes

---
+++ Cinetopia/Cinetopia/Models/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  Cinetopia
+//
+//  Created by Bianca Maciel Matos on 14/04/24.
+//
+
+import Foundation
+
+
+struct Movie {
+    let id: Int
+    let title: String
+    let image: String
+    let synopsis: String
+    let rate: Double
+    let releaseDate: String
+}
